f343cd1310e2120252018169b5c04a98af1ba125
Thadryan <thadryan@protonmail.com>
2026-07-14 13:49:02 -0400

    Guard COQnow call so startup doesn't crash

--- dot_config/nvim/lua/plugins/lsp-and-completion/coqnvim.lua
+++ dot_config/nvim/lua/plugins/lsp-and-completion/coqnvim.lua
@@ -1,12 +1,12 @@
 return {
     "ms-jpq/coq_nvim",
-    -- enabled = false,
+    enabled = true,
     lazy = false,
     -- I can't figure out how to do this to get the error to go away,
     -- https://github.com/ms-jpq/coq_nvim/issues/411, so I just use the work around for now
     config = function()
         require("coq")
-        vim.cmd("COQnow --shut-up")
+        pcall(vim.cmd, "COQnow --shut-up")
         -- remap accept to ctrl-k
         -- includes the files in the gitignore by default
         -- Map Tab to accept the current autocomplete suggestion
