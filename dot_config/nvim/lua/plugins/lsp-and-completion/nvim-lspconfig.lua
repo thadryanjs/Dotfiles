80b84c242169c139ef487cbcf0ea0afc32baac84
Thadryan <thadryan@protonmail.com>
2026-07-14 14:25:26 -0400

    Disable coq, add native LSP completion setup

--- dot_config/nvim/lua/plugins/lsp-and-completion/nvim-lspconfig.lua
+++ dot_config/nvim/lua/plugins/lsp-and-completion/nvim-lspconfig.lua
@@ -51,5 +51,16 @@ return {
       "jdtls",
       -- Remove or add more as needed, only if config exists
     })
+
+    -- native LSP auto-completion (replaces coq)
+    vim.api.nvim_create_autocmd("LspAttach", {
+      callback = function(ev)
+        local client = vim.lsp.get_client_by_id(ev.data.client_id)
+        if client and client:supports_method("textDocument/completion") then
+          vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
+        end
+      end,
+    })
   end,
 }
+
